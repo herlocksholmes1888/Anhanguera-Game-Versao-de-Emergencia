dea8178e8b0c9eecc780b3fdbc2181cc75413b9e
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-04-18 15:39:33 -0300

    :construction: add choice class

--- src/game/Escolha.java
+++ src/game/Escolha.java
@@ -1,5 +1,27 @@
 package game;
 
 public class Escolha {
-
+	private int indexEscolha;
+	private int mudancaSatisfacao;
+	
+	public Escolha(int indexEscolha) {
+		this.setIndexEscolha(indexEscolha);
+		this.setMudancaSatisfacao(mudancaSatisfacao);
+	}
+	
+	public void setIndexEscolha(int indexEscolha) {
+		this.indexEscolha = indexEscolha;
+	}
+	
+	public int getIndexEscolha() {
+		return indexEscolha;
+	}
+	
+	public void setMudancaSatisfacao(int mudancaSatisfacao) {
+		this.mudancaSatisfacao = mudancaSatisfacao;
+	}
+	
+	public int getMudancaSatisfacao() {
+		return mudancaSatisfacao;
+	}
 }
--- src/game/Main.java
+++ src/game/Main.java
@@ -3,6 +3,6 @@ package game;
 public class Main {
 
 	public static void main(String[] args) {
-		Personagem jogador = new Personagem("Luisa", 3);
+		
 	}
 }
