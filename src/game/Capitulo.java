b18d330bd41c1e5bbf603f0d677cb0deb8fb7917
Luisa Frugoli <luisafrugoli11@gmail.com>
2024-04-18 15:43:44 -0300

    :construction: add chapter class

--- src/game/Capitulo.java
+++ src/game/Capitulo.java
@@ -1,5 +1,40 @@
 package game;
 
-public class Capitulo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Capitulo {
+	private int numeroCapitulo;
+	private String tituloCapitulo;
+	private List<String> dialogos;
+	
+	public Capitulo(int numeroCapitulo, String tituloCapitulo) {
+		this.numeroCapitulo = numeroCapitulo;
+	    this.tituloCapitulo = tituloCapitulo;
+	    this.dialogos = new ArrayList<>();
+	}
+
+	public int getNumeroCapitulo() {
+	    return numeroCapitulo;
+	}
+
+	public void setNumeroCapitulo(int numeroCapitulo) {
+	    this.numeroCapitulo = numeroCapitulo;
+	}
+
+	public String getTituloCapitulo() {
+	     return tituloCapitulo;
+	}
+
+	public void setTituloCapitulo(String tituloCapitulo) {
+	     this.tituloCapitulo = tituloCapitulo;
+	}
+
+	public List<String> getDialogos() {
+	     return dialogos;
+	}
+
+	public void setDialogos(List<String> dialogos) {
+	     this.dialogos = dialogos;
+	}
 }
